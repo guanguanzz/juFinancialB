fdd86390f2b135d592a531ef7ee3199f9fbfef34
xjdxc <290454691@qq.com>
2019-06-11 23:08:26 +0800

    1

--- src/api/OperatManage/ContentManage.js
+++ src/api/OperatManage/ContentManage.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 var getList = function(title,type,status,creatBy,creatAt,){
     return Axios.get('/api/a/u/content/query',{
-        title,
+        title:title,
         itype:type,
         status,
         update_by:creatBy,
